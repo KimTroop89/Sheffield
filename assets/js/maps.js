00034bc66b132238614ee39b9048e0c7d3c264e1
KimTroop89 <Cawkwell14@hotmail.com>
2020-06-14 09:40:45 +0000

    Added Google Maps with Markers - not loading so need to look at code

---
+++ assets/js/maps.js
@@ -0,0 +1,31 @@
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: {
+                    lat: 53.381128,
+                    lng: -1.470085
+                }
+            });
+        } 
+
+        var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        var locations = [
+            { lat: 53.379830, lng: -1.468137 },
+            { lat: 53.379830, lng: -1.468137 },
+            { lat: 53.379830, lng: -1.468137 }
+        ];
+
+        var markers = locations.map(function(location, i){
+            return new google.maps.Marker({
+                position: location,
+                label: labels[i % labels.length]
+            });
+        });
+
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+
+   
+    
