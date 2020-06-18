7e0a902b93d744ef9095c2bd0b065ccc8e03e87a
KimTroop89 <Cawkwell14@hotmail.com>
2020-06-18 16:03:26 +0000

    Added top 5 sections

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,6 @@
         function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
+                zoom: 5,
                 center: {
                     lat: 53.381128,
                     lng: -1.470085
