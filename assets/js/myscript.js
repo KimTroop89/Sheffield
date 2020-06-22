3740d488397042ad82ee49fb327e551dc9bca8b2
KimTroop89 <Cawkwell14@hotmail.com>
2020-06-22 10:20:15 +0000

    added jquery file and linked in html

---
+++ assets/js/myscript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $(".to-do").ready(function(){
+        $(".to-do").hide();
+    });
+
+    $(".shopping-header").click(function(){
+        $(".to-do").show();
+    });
+
+    $(".").click(function(){
+        $(".to-do").hide();
+    });
+
+});
